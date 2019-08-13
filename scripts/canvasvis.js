db27ae4720473a72420a24f4870bfca2ef8d273c
T.J. Kirchner <tkirchner@tenable.com>
2019-08-13 10:27:56 -0700

    initial color sequencing implementation

--- scripts/canvasvis.js
+++ scripts/canvasvis.js
@@ -2,6 +2,8 @@ console.clear();
 // import framework
 import Vis from '/modules/Vis.js';
 
+item 8
+
 // setup
 const visEl = document.querySelector('#visual');
 // setup 2D canvas plus resize
@@ -13,12 +15,12 @@ function Dimensions() {
 	this.height = (window.innerHeight)*dpr;
 	this.centerX = this.width/2;
 	this.centerY = this.height/2;
-	
+
 	this.setFullscreen = function(el) {
 		el.width = this.width;
 		el.height = this.height;
 	}
-	
+
 	this.update = function() {
 		this.width = (window.innerWidth)*dpr;
 		this.height = (window.innerHeight)*dpr;
@@ -42,21 +44,62 @@ const binSize = 128;
 const elAmount = Math.floor(binSize/3); // Returned frequncies is a third
 
 // create a new vis
-const vis = new Vis(binSize, '/beast.mp3');
+// const vis = new Vis(binSize, '/beast.mp3');
+const vis = new Vis(binSize, '131593389');
+
+
+
+
+const initColor = [0, 128, 128]; // lavender
+
+let color = [...initColor];
+let colorIndex = 0;
+let colorIndex2 = 1;
 
 // setup our draw loop: THIS IS WHERE THE MAGIC HAPPENS!!
 vis.draw( () => {
+
+	++color[colorIndex];
+	--color[colorIndex2];
+
+	if (color[colorIndex] >= 255) {
+		if (colorIndex == color.length - 1) {
+			colorIndex = 0;
+			color = [...initColor];
+		} else {
+			colorIndex++;
+		}
+	}
+
+	if (color[colorIndex2] <= 0) {
+		if (colorIndex2 <= 0) {
+			colorIndex2 = color.length - 1;
+		} else {
+			colorIndex2--;
+		}
+	}
+
+	if (colorIndex === colorIndex2) {
+		if (colorIndex === 0) {
+            colorIndex2 = color.length - 1;
+        } else if (colorIndex === color.length - 1) {
+			colorIndex = 0;
+		} else {
+			colorIndex++;
+		}
+	}
 
 	ctx.clearRect(0, 0, screenDim.width, screenDim.height);
 	// loop over our frequencies and draw a shape for each one
 	vis.frequencies.forEach((f, i) => {
 		ctx.beginPath();
-		ctx.fillStyle = 'red';
+		ctx.fillStyle = 'rgb(0, 255, 0)';
+		ctx.fillStyle = `rgb(${color.join(', ')}`;
 		ctx.arc(i*20, i*20, f, 0, 5);
 		ctx.fill();
 		ctx.closePath();
 	})
-	
+
 } )
 
 
@@ -72,5 +115,5 @@ controls.querySelector('[data-control="play"]').addEventListener('click', functi
     this.dataset.on = "false";
     vis.stop();
 	}
-   
+
 })
